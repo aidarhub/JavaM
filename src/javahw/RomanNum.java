67aa88f0ee13974a18e0e23b7c438e7dde0dc15f
aidarhub <68774520+aidarhub@users.noreply.github.com>
2020-10-26 21:49:53 +0600

    Commit

--- src/javahw/RomanNum.java
+++ src/javahw/RomanNum.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public enum RomanNum {
-    I(1), IV(4), V(5), IX(9), X(10);
+    I(1), IV(4), V(5), IX(9), X(10), L(50), C(100), D(500), M(1000);
 
     private int value;
 
